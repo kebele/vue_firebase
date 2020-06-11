351f8274119f144dada99c7e3bd11fa70f493724
kebele <ykemalbeler@gmail.com>
2020-06-12 02:13:32 +0300

    ekleme

---
+++ vuefire/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+// import VueFire from 'vuefire'
+import App from './App.vue'
+
+// Vue.use(VueFire)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
